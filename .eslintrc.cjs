1a0131c246c142434368341a5f7fce198abc3410
huydtq <huy.do@indigames.net>
2023-08-18 16:34:02 +0700

    chore: setup eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,124 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+    ecmaFeatures: {
+      jsx: true
+    },
+    tsconfigRootDir: './'
+  },
+  plugins: ['react', 'react-refresh', 'import', 'prettier'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'linebreak-style': 'off',
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'import/no-unresolved': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'] }],
+    'react/jsx-sort-props': [
+      2,
+      {
+        callbacksLast: true,
+        shorthandFirst: false,
+        shorthandLast: true,
+        multiline: 'last',
+        ignoreCase: true,
+        noSortAlphabetically: false
+      }
+    ],
+    'react-hooks/exhaustive-deps': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        json: 'never'
+      }
+    ],
+    'class-methods-use-this': ['error', { enforceForClassFields: false }],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        arrowParens: 'always',
+        bracketSpacing: true,
+        jsxBracketSameLine: false,
+        jsxSingleQuote: true,
+        quoteProps: 'as-needed',
+        singleQuote: true,
+        semi: false,
+        printWidth: 120,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'none'
+      },
+      {
+        usePrettierrc: false,
+        fileInfoOptions: {
+          withNodeModules: false
+        }
+      }
+    ],
+    'sort-imports': ['error', { ignoreCase: true, ignoreDeclarationSort: true }],
+    'import/order': [
+      'error',
+      {
+        groups: [['external', 'builtin'], 'internal', ['sibling', 'parent'], 'index'],
+        pathGroups: [
+          {
+            pattern: '@(react|react-native)',
+            group: 'external',
+            position: 'before'
+          },
+          {
+            pattern: '@src/**',
+            group: 'internal'
+          }
+        ],
+        pathGroupsExcludedImportTypes: ['internal', 'react'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        }
+      }
+    ]
   },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx', '.js', '.jsx', '.json']
+    },
+    'import/resolver': {
+      alias: true,
+      typescript: {},
+      project: './tsconfig.json',
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
+      }
+    },
+    react: {
+      version: 'detect'
+    },
+    'import/ignore': ['node_modules']
+  }
 }
