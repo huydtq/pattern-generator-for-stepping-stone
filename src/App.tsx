7d62ade5ae1b26a5f47789f26b3ddfb968616235
huydtq <huy.do@indigames.net>
2023-08-18 18:34:47 +0700

    feat: add canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+
+import { Box, OrbitControls, OrthographicCamera, Stats } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { useControls } from 'leva'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const mainCameraRef = React.useRef<THREE.PerspectiveCamera | undefined>(undefined)
+
+  const rotationOptions = React.useMemo(() => {
+    return {
+      x: { value: 0, min: -360, max: 360, step: 0.01 },
+      y: { value: 0, min: -360, max: 360, step: 0.01 },
+      z: { value: 0, min: -360, max: 360, step: 0.01 }
+    }
+  }, [])
+
+  const rotation = useControls('Rotation', rotationOptions)
 
   return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
-    </>
+    <div
+      style={{
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: 'gray'
+      }}
+    >
+      <Canvas
+        gl={{
+          antialias: true
+        }}
+        style={{
+          height: '100vh',
+          width: '100vw',
+          backgroundColor: 'yellow'
+        }}
+      >
+        <OrthographicCamera
+          position={[0, 0, 10]}
+          ref={mainCameraRef}
+          rotation={[rotation.x, rotation.x, rotation.z]}
+          zoom={200}
+          makeDefault
+        />
+        <ambientLight intensity={0.5} />
+        <Box attach='material' position={[0, 0, 0]}>
+          <meshNormalMaterial />
+        </Box>
+        <OrbitControls camera={mainCameraRef.current} />
+        <Stats />
+      </Canvas>
+    </div>
   )
 }
-
-export default App
